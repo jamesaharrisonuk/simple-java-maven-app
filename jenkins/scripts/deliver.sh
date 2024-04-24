fd182eec0e6e08934a8d2a28532892a472facfc2
James Harrison <32297135+jamesaharrisonuk@users.noreply.github.com>
2024-04-24 15:44:16 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -20,6 +20,11 @@ set -x
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
+set -x
+echo 'Full directory listing of repo'
+find .
+set +x
+
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
