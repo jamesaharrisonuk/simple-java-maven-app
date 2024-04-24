b5cb7c3ef3fbad83f0907853793149081daa2305
James Harrison <32297135+jamesaharrisonuk@users.noreply.github.com>
2024-04-24 17:41:49 +0100

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
 
-DIR=$(pwd)
-
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
-
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
-
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
-
 NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | sed -e "s/\x1b\[.\{1,5\}m//g")
-
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
-
 VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | sed -e "s/\x1b\[.\{1,5\}m//g")
-
-
-
-echo 'Full directory listing of repo'
-find .
-
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
